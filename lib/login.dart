45fe64fcb14d6c3122de308f23df1467e0f629ec
Ishaan Arya <66945981+ishaan-arya@users.noreply.github.com>
2023-11-18 14:07:17 -0500

    ft[login.dart] => 🎫 initialised named routes

---
+++ lib/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(children: [Text("Note Mesh")]),
+      ),
+    );
+  }
+}
