763c27179db6ad6145252b6458d8bbc1d95d4d8a
Ishaan Arya <66945981+ishaan-arya@users.noreply.github.com>
2023-11-18 18:01:57 -0500

    ft[files] => 📁 added file picker for ios

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 const Color kPrimaryGreen = Color(0xffD8FFB1);
 
+const kHeadingStyle = TextStyle(fontFamily: "RobotoMono", fontSize: 32);
+
+var kGreenBoxDecoration = BoxDecoration(
+  color: Color(0xffD8FFB1),
+  borderRadius: BorderRadius.circular(15),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.grey.shade600,
+      offset: const Offset(
+        5.0,
+        5.0,
+      ),
+      blurRadius: 3.0,
+      spreadRadius: 0.5,
+    ), //BoxShadow
+  ],
+);
+
 const kTextFieldDecoration = InputDecoration(
   contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
   border: OutlineInputBorder(
